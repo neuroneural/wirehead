fd0fe73e1282ddbe8455f37c540eee355bdf44f4
spikedoanz <manhdoanmainmail@gmail.com>
2023-10-06 18:17:39 -0400

    fixed concurrency issues

---
+++ dev_src/wirehead.py
@@ -0,0 +1,65 @@
+import redis
+import time
+import os
+import pickle
+import sys
+import random
+
+# Things that users should change
+DEFAULT_HOST = 'localhost'
+DEFAULT_PORT = 6379
+DEFAULT_CAP = 1000
+MANAGER_TIMEOUT = 1
+
+
+# Things that users should definitely NOT change
+ERROR_STRING= """Oppsie Woopsie! Uwu Redwis made a shwuky wucky!! A widdle
+bwucko boingo! The code monkeys at our headquarters are
+working VEWY HAWD to fix this!"""
+PATH_TO_DATA = "/data/users1/mdoan4/wirehead/synthseg/data/training_label_maps/"
+DATA_FILES = ["training_seg_01.nii.gz",  "training_seg_02.nii.gz",  "training_seg_03.nii.gz",  "training_seg_04.nii.gz",  "training_seg_05.nii.gz",  "training_seg_06.nii.gz",  "training_seg_07.nii.gz",  "training_seg_08.nii.gz",  "training_seg_09.nii.gz",  "training_seg_10.nii.gz",  "training_seg_11.nii.gz",  "training_seg_12.nii.gz",  "training_seg_13.nii.gz",  "training_seg_14.nii.gz",  "training_seg_15.nii.gz", "training_seg_16.nii.gz", "training_seg_17.nii.gz", "training_seg_18.nii.gz","training_seg_19.nii.gz", "training_seg_20.nii.gz",]
+
+def get_queue_len(host = DEFAULT_HOST, port = DEFAULT_PORT):
+    try:
+        r = redis.Redis(host= host, port=DEFAULT_PORT, db=0)
+        return r.llen('db0'), r.llen('db1')
+    except:
+        return -1, -1
+
+def quantize_to_uint8(tensor):
+    tensor = ((tensor - tensor.min())/(tensor.max() - tensor.min())*255).round() 
+    return tensor.astype('uint8') 
+
+def lock_db(r, lock_name, timeout=10):
+    while True:
+        if r.setnx(lock_name, 1):
+            r.expire(lock_name, timeout)
+            return True
+        time.sleep(0.1)
+
+def swap_db(r):
+    lock_name = 'swap_lock'
+    locked = lock_db(r, lock_name = lock_name)
+    if locked:
+        try:
+            pipe = r.pipeline()
+            if r.exists("db0") and r.exists("db1"):
+                pipe.multi()
+                pipe.rename("db0", "temp_key")
+                pipe.rename("db1", "db0")
+                pipe.rename("temp_key", "db1")
+                pipe.delete('db1')
+                pipe.execute()
+        finally:
+            r.delete(lock_name)
+
+def push_db(r, package_bytes):
+    lock_name = 'swap_lock'
+    locked = lock_db(r, lock_name = lock_name)
+    if locked:
+        try:
+            r.rpush("db1", package_bytes)
+            if not r.exists("db0"):
+                r.rpush("db0", package_bytes)
+        finally:
+            r.delete(lock_name)
