d5c926042dd2dc54524d4116d4e722bb7881451b
spikedoanz <manhdoanmainmail@gmail.com>
2023-10-10 16:43:21 -0400

    got some work done on dataloader

---
+++ dataloader.py
@@ -0,0 +1,19 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+import time
+import redis
+import numpy as np
+import pickle
+import wirehead as wh
+
+def identity(sample):
+    return sample
+if __name__ == "__main__":
+    dataset = wh.wirehead_dataloader_v1(transform = identity)
+    dataloader = DataLoader(dataset, batch_size=1)
+    for batch in dataloader:
+        im, lab = batch[0], batch[1]
+        print("Dataloader: Fetched im and lab successfully")
+
+        
+  
---
+++ dev_start.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+CAP=30
+PORT=6379
+
+export PATH=$PATH:/data/users1/mdoan4/wirehead/redis/redis-stable/src/
+
+
+echo $HOSTNAME >&2
+
+# Start redis
+/data/users1/mdoan4/wirehead/redis/redis-stable/src/redis-server /data/users1/mdoan4/wirehead/dev_src/redis.conf >> ./log/manager_wirehead_output.log 2>> ./log/manager_wirehead_errors.log &
+REDIS_PID=$!
+
+sleep 2 
+
+redis-cli flushall
+LOCAL_IP=$(hostname -I | awk '{print $1}')
+echo $LOCAL_IP
+
+# Start python scripts
+trap 'pkill -P $$' EXIT
+source /data/users1/mdoan4/anaconda3/etc/profile.d/conda.sh
+conda activate wirehead
+python /data/users1/mdoan4/wirehead/dev_src/redis_status.py --ip $LOCAL_IP $SLURM_ARRAY_TASK_ID 
+
+python /data/users1/mdoan4/wirehead/dev_src/generate.py --ip $LOCAL_IP $SLURM_ARRAY_TASK_ID &
+python /data/users1/mdoan4/wirehead/dev_src/manager.py --ip $LOCAL_IP --cap $CAP $SLURM_ARRAY_TASK_ID #&
+#python /data/users1/mdoan4/wirehead/dev_src/dataloader.py --ip $LOCAL_IP $SLURM_ARRAY_TASK_ID 
+
+
+# Cleanup 
+kill $REDIS_PID
+
+
+wait
+
+
+
