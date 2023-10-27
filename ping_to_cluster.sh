9b2cca7992519f8941d5137c32b41e5740b7aba8
spikedoanz <manhdoanmainmail@gmail.com>
2023-10-27 11:36:20 -0400

    got slurm jobs working

--- ping_to_cluster.sh
+++ ping_to_cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp -r ./* arcdev101:/data/users1/mdoan4/wirehead/
+scp -r ./* arcdev101:/data/users1/mdoan4/wirehead/dev_src/
 
---
+++ start_wirehead.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Launch the two shell scripts
+./wirehead_server.sh &
+./wirehead_train.sh
+
