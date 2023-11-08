40995c774b9c81fbc2f5bfd76ddf1011b9a912ba
Mike Doan <manhdoanmainmail@gmail.com>
2023-11-08 10:55:07 -0500

    made everything work on slurm, need to test on diferent person's setup

--- wirehead_generate.sh
+++ wirehead_generate.sh
@@ -5,9 +5,8 @@ LOCAL_IP='localhost'
 
 # Start python scripts
 trap 'pkill -P $$' EXIT
-source /data/users1/mdoan4/anaconda3/etc/profile.d/conda.sh
+source /data/users1/mdoan4/wirehead/envs/wirehead_generate/bin/activate
 
-conda activate wirehead_generate
 python /data/users1/mdoan4/wirehead/src/generate.py --ip $LOCAL_IP $SLURM_ARRAY_TASK_ID  
 
 echo "Generator: Terminated"
