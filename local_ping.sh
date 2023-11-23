aaee225a8a261f7e28484d31b70a8907ca978dc7
Mike Doan <manhdoanmainmail@gmail.com>
2023-11-23 09:01:17 -0500

    adjusted post-processing on dataloader side

---
+++ local_ping.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scp -r arcdev101:/data/users1/mdoan4/wirehead/src ./
+scp -r arcdev101:/data/users1/mdoan4/wirehead/dev_src ./
+scp -r arcdev101:/data/users1/mdoan4/wirehead/training_scripts ./
