560f6d9c3ed4bd3fc77e306ffe0bc22e3642caca
Mike Doan <manhdoanmainmail@gmail.com>
2024-02-12 16:43:29 -0500

    x

--- src/utils/do_nothing.py
+++ src/utils/do_nothing.py
@@ -2,10 +2,11 @@ import torch
 import redis
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
+import pickle
 import time
 
 
-DEFAULT_HOST = '10.245.11.255' #arctrdcn017
+DEFAULT_HOST = 'arctrdcn017' #arctrdcn017
 DEFAULT_PORT = 6379
 WIREHEAD_NUMSAMPLES=10
 def min_max_normalize(x):
@@ -31,8 +32,8 @@ def rcollate(batch, size=256):
     np.save('./sample.npy', sample)
     np.save('./label.npy', label)
     '''
-    data = quantile_normalize(torch.from_numpy(batch[0][0]).float()).unsqueese(1)
-    labels = torch.from_numpy(batch[0][1]).long().unsqueese(1)
+    data = quantile_normalize(torch.from_numpy(batch[0][0]).float()).unsqueeze(1)
+    labels = torch.from_numpy(batch[0][1]).long().unsqueeze(1)
     return data.unsqueeze(1), labels
 
 
@@ -64,7 +65,6 @@ class whDataset(Dataset):
         return self.num_samples
     def __getitem__(self, index):
         r = redis.Redis(host=self.host, port=self.port)
-        print('ding')
         index = index % r.llen(self.db_key)  # Use modular arithmetic to cycle through dataset
         pickled_data = r.lindex(self.db_key, index)
         if pickled_data is not None:
@@ -79,8 +79,7 @@ dataset = whDataset(host=DEFAULT_HOST,port=DEFAULT_PORT, transform=rtransform,nu
 dataloader = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=rcollate)
 
 # Serve samples in batches from the DataLoader
-for batch_idx, (data, target) in enumerate(dataloader):
-    print(batch_idx)
+for batch_idx, (data, target) in tqdm(enumerate(dataloader)):
     # Here you can use the data and target for your model training
     pass
 
