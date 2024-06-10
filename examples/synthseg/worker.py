96c0aec4c132d77957a0a30ce6e56c893aca3af8
Mike <mdoan4@gsu.edu>
2024-06-10 18:50:27 +0000

    label map

--- examples/synthseg/worker.py
+++ examples/synthseg/worker.py
@@ -9,22 +9,24 @@ PATH_TO_DATA        = ("/data/users1/mdoan4/wirehead/dependencies/synthseg/data/
 DATA_FILES          = [f"training_seg_{i:02d}.nii.gz" for i in range(1, 21)]
 PATH_TO_SYNTHSEG    = '/data/users1/mdoan4/wirehead/dependencies/synthseg'
 
+LABEL_MAP = np.asarray(
+    [0, 0, 1, 2, 3, 4, 0, 5, 6, 0, 7, 8, 9, 10]
+    + [11, 12, 13, 14, 15]
+    + [0] * 6
+    + [1, 16, 0, 17]
+    + [0] * 12
+    + [18, 19, 20, 21, 0, 22, 23]
+    + [0, 24, 25, 26, 27, 28, 29, 0, 0, 18, 30, 0, 31]
+    + [0] * 75
+    + [3, 4]
+    + [0] * 25
+    + [20, 21]
+    + [0] * 366,
+    dtype="int",
+).astype(np.uint8)
+
+
 def preprocess_label(lab, label_map=LABEL_MAP):
-    LABEL_MAP = np.asarray(
-        [0, 0, 1, 2, 3, 4, 0, 5, 6, 0, 7, 8, 9, 10]
-        + [11, 12, 13, 14, 15]
-        + [0] * 6
-        + [1, 16, 0, 17]
-        + [0] * 12
-        + [18, 19, 20, 21, 0, 22, 23]
-        + [0, 24, 25, 26, 27, 28, 29, 0, 0, 18, 30, 0, 31]
-        + [0] * 75
-        + [3, 4]
-        + [0] * 25
-        + [20, 21]
-        + [0] * 366,
-        dtype="int",
-    ).astype(np.uint8)
     return label_map[lab.astype(np.uint8)]
 
 def preprocess_image_min_max(img: np.ndarray) -> np.ndarray:
