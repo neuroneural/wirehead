177dbe3b944e631cd3d4182f1c7c7506fa778cd5
spikedoanz <spikedoanz@gmail.com>
2024-06-11 15:55:49 -0400

    synthsegggg

--- examples/synthseg/deploy_worker.sh
+++ examples/synthseg/deploy_worker.sh
@@ -12,35 +12,10 @@
 #SBATCH -A psy53c17
 #SBATCH --array=0-2
 
-echo "This is a test job running on node $(hostname)"
+echo "This is a Synthseg generation job running on node $(hostname)"
 echo "Error output test" >&2
 
 conda init 
 conda activate wirehead_generate
 
-# Function to terminate child processes
-terminate_child_processes() {
-    # Send SIGTERM to all child processes
-    pkill -SIGTERM -P $$
-}
-
-# Function to check if MongoDB is running
-check_mongo() {
-    if pgrep -x "mongod" > /dev/null
-    then
-        echo "MongoDB is running."
-    else
-        echo "MongoDB is not running. Please start mongod according to your system specs"
-        kill -SIGINT $$
-    fi
-}
-
-check_mongo
-
-# Trap the SIGINT signal (Ctrl+C)
-trap terminate_child_processes SIGINT
-
-stdbuf -o0 python manager.py &
-stdbuf -o0 python generator.py
-
-
+stdbuf -o0 python worker.py
