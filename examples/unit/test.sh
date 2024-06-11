81789a46af42aa687a34423fcafdd7c5073ccd76
spikedoanz <spikedoanz@gmail.com>
2024-06-11 13:38:05 -0400

    unit test

--- examples/unit/test.sh
+++ examples/unit/test.sh
@@ -1,24 +1,52 @@
 #!/bin/bash
 
+# Function to check if MongoDB is running
+check_mongo() {
+    if pgrep -x "mongod" > /dev/null
+    then
+        echo "MongoDB is running."
+    else
+        echo "MongoDB is not running. Starting MongoDB..."
+        mongod &
+        sleep 5  # Wait for MongoDB to start
+        echo "MongoDB started."
+    fi
+}
+
+# Function to run clean.py
 run_clean() {
     python clean.py
 }
-# Function to run test.py
-run_test() {
-    python test.py
+
+run_manager() {
+    python manager.py
+}
+
+run_generator() {
+    python generator.py
 }
 
 # Function to run loader.py
 run_loader() {
     python loader.py
     # Send a signal to the test.py process to terminate it
-    pkill -f test.py
+    pkill -f manager.py
+    pkill -f generator.py
 }
 
-# Run test.py in the background
+# Check if MongoDB is running and start it if necessary
+check_mongo
+
+# Run clean.py
 run_clean
-run_test &
-test_pid=$!
+echo
+
+# Run test.py in the background
+run_manager&
+manager_pid=$!
+
+run_generator&
+generator_pid=$!
 
 # Run loader.py in the foreground
 run_loader
@@ -27,6 +55,10 @@ run_loader
 wait $!
 
 # Print a message indicating that both scripts have finished
+
+pkill -f manager.py
+pkill -f generator.py
+
 echo
 echo
 echo "-----------"
