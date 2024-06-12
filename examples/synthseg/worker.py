9722b4a4add737dfeb095fdc75c9a0326efc78d6
spikedoanz <spikedoanz@gmail.com>
2024-06-12 14:05:07 -0400

    added merge homologs

--- examples/synthseg/worker.py
+++ examples/synthseg/worker.py
@@ -25,6 +25,27 @@ LABEL_MAP = np.asarray(
     dtype="int",
 ).astype(np.uint8)
 
+def merge_homologs(label):#, device):
+    max_value = 31
+    # idx = torch.arange(max_value + 1, dtype=torch.long).to(device)
+    idx = np.arange(max_value+1)
+    idx[31] = 17
+    idx[30] = 16
+    idx[29] = 15
+    idx[28] = 14
+    idx[27] = 10
+    idx[26] = 9
+    idx[25] = 8
+    idx[24] = 7
+    idx[23] = 6
+    idx[22] = 5
+    idx[21] = 4
+    idx[20] = 3
+    idx[19] = 2
+    idx[18] = 1
+    # return the corresponding values from idx
+    return idx[label]
+
 def preprocess_label(lab, label_map=LABEL_MAP):
     return label_map[lab.astype(np.uint8)]
 
@@ -39,6 +60,8 @@ def preprocessing_pipe(data):
     img = preprocess_image_min_max(img) * 255
     img = img.astype(np.uint8)
     lab = preprocess_label(lab)
+    lab = merge_homologs(lab)
+    lab = lab.astype(np.uint8)    
     return (img, lab) 
 
 def hardware_setup():
