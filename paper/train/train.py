f5c383553f7a7b775ee992fd9ab962e167350a34
spikedoanz <spikedoanz@gmail.com>
2024-06-13 15:52:16 -0400

    gca

--- paper/train/train.py
+++ paper/train/train.py
@@ -1,41 +1,40 @@
+import os
+from datetime import datetime
+import shutil
+import sys
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
+
 from model import UNet
 from dice import DiceLoss
+from misc import RandomDataset, Logger
+
+# Save model, config and output
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
+log_dir = f"./log/{timestamp}"
+os.makedirs(log_dir, exist_ok=True)
+model_path = os.path.join(log_dir, "unet_model.pth")
+train_script_path = os.path.join(log_dir, "train.py")
+output_path = os.path.join(log_dir, "output.txt")
+sys.stdout = Logger(output_path)
 
 
 # Hyperparameters
-num_epochs = 10
+num_epochs = 1
 batch_size = 1
 learning_rate = 0.001
 n_channels = 1
 n_classes = 2
 
-# Custom dataset
-class RandomDataset(Dataset):
-    def __init__(self, num_samples):
-        self.num_samples = num_samples
-
-    def __len__(self):
-        return self.num_samples
-
-    def __getitem__(self, idx):
-        n = 64
-        shape = (n,n,n)
-        input_data = torch.rand(*shape, dtype=torch.float32)
-        label_data = torch.randint(0, 2, shape, dtype=torch.int32)
-        return input_data, label_data
-
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 # Initialize the model, loss function, and optimizer
 model = UNet(n_channels=n_channels, n_classes=n_classes).to(device)
 criterion = DiceLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
 # Create the dataset and dataloader
 dataset = RandomDataset(num_samples=100)
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
@@ -61,5 +60,9 @@ for epoch in range(num_epochs):
         if (batch_idx + 1) % 10 == 0:
             print(f"Epoch [{epoch+1}/{num_epochs}], Batch [{batch_idx+1}/{len(dataloader)}], Loss: {loss.item():.4f}")
 
-# Save the trained model
-torch.save(model.state_dict(), 'unet_model.pth')
+
+# Save to logdir
+torch.save(model.state_dict(), model_path)
+shutil.copy("train.py", train_script_path)
+
+print(f"Model weights, train.py script and output saved in: {log_dir}")
