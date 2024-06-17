b59f93e5abcabed2181dc94bcd48f385aaa1aeee
spikedoanz <spikedoanz@gmail.com>
2024-06-17 14:29:20 -0400

    wirehead benchmark ready

---
+++ paper/wirehead_train/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Function to terminate child processes
+terminate_child_processes() {
+    # Send SIGTERM to all child processes
+    pkill -SIGTERM -P $$
+}
+
+# Function to check if MongoDB is running
+check_mongo() {
+    if pgrep -x "mongod" > /dev/null
+    then
+        echo "MongoDB is running."
+    else
+        echo "MongoDB is not running. Please start mongod according to your system specs"
+        kill -SIGINT $$
+    fi
+}
+
+check_mongo
+
+# Trap the SIGINT signal (Ctrl+C)
+trap terminate_child_processes SIGINT
+
+python clean.py
+
+python manager.py &
+
+python generator.py &
+
+python train.py
+
+kill -SIGINT $$
