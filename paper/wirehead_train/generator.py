b1e5993f25e4d12be1a068af4da05abef4fe11ce
spikedoanz <spikedoanz@gmail.com>
2024-06-17 20:07:40 +0000

    a100 1x naive

--- paper/wirehead_train/generator.py
+++ paper/wirehead_train/generator.py
@@ -1,5 +1,7 @@
 import sys
 import numpy as np
+import torch
+from torch.utils.data import Dataset
 from wirehead import WireheadManager, WireheadGenerator
 
 # Synthseg config
@@ -7,6 +9,61 @@ WIREHEAD_CONFIG     = "config.yaml"
 PATH_TO_DATA        = "./SynthSeg/data/training_label_maps/"
 DATA_FILES          = [f"training_seg_{i:02d}.nii.gz" for i in range(1, 21)]
 PATH_TO_SYNTHSEG    = './SynthSeg'
+
+LABEL_MAP = np.asarray(
+    [0, 0, 1, 2, 3, 4, 0, 5, 6, 0, 7, 8, 9, 10]
+    + [11, 12, 13, 14, 15]
+    + [0] * 6
+    + [1, 16, 0, 17]
+    + [0] * 12
+    + [18, 19, 20, 21, 0, 22, 23]
+    + [0, 24, 25, 26, 27, 28, 29, 0, 0, 18, 30, 0, 31]
+    + [0] * 75
+    + [3, 4]
+    + [0] * 25
+    + [20, 21]
+    + [0] * 366,
+    dtype="int",
+).astype(np.uint8)
+
+def merge_homologs(label):#, device):
+    max_value = 31
+    # idx = torch.arange(max_value + 1, dtype=torch.long).to(device)
+    idx = np.arange(max_value+1)
+    idx[31] = 17
+    idx[30] = 16
+    idx[29] = 15
+    idx[28] = 14
+    idx[27] = 10
+    idx[26] = 9
+    idx[25] = 8
+    idx[24] = 7
+    idx[23] = 6
+    idx[22] = 5
+    idx[21] = 4
+    idx[20] = 3
+    idx[19] = 2
+    idx[18] = 1
+    # return the corresponding values from idx
+    return idx[label]
+
+def preprocess_label(lab, label_map=LABEL_MAP):
+    return label_map[lab].astype(np.uint8)
+
+def preprocess_image_min_max(img: np.ndarray):
+    "Min max scaling preprocessing for the range 0..1"
+    img = (img - img.min()) / (img.max() - img.min())
+    return img
+
+def preprocessing_pipe(data):
+    """ Set up your preprocessing options here, ignore if none are needed """
+    img, lab = data
+    img = preprocess_image_min_max(img) * 255
+    img = img.astype(np.uint8)
+    lab = preprocess_label(lab)
+    lab = merge_homologs(lab)
+    lab = lab.astype(np.uint8)    
+    return (img, lab) 
 
 def hardware_setup():
     """ Clean slate to set up your hardware, ignore if none are needed """
@@ -22,7 +79,7 @@ def hardware_setup():
     pass
 
 # Create a generator function that yields desired samples
-def create_generator(n_samples = 1000, task_id = 0, training_seg=None):
+def create_generator(task_id = 0, training_seg=None):
     """ Creates an iterator that returns data for mongo.
         Should contain all the dependencies of the brain generator
         Preprocessing should be applied at this phase 
@@ -37,8 +94,8 @@ def create_generator(n_samples = 1000, task_id = 0, training_seg=None):
     brain_generator = BrainGenerator(PATH_TO_DATA + training_seg)
     print(f"Generator: SynthSeg is generating off {training_seg}",flush=True,)
     # 2. Run your generator in a loop, and pass in your preprocessing options
-    for i in range(n_samples):
-        img, lab = brain_generator.generate_brain()
+    for i in range(1000):
+        img, lab = preprocessing_pipe(brain_generator.generate_brain())
         print(f"Generator: Unique sample {i}")
         # 3. Yield your data, which will automatically be pushed to mongo
         yield (img, lab)
