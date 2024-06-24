b25f730594bf77bcea99f8c9270ed02c93f809a1
spikedoanz <spikedoanz@gmail.com>
2024-06-24 19:23:37 +0000

    deploy workers loads from env variables

--- paper/multigpu_gen/gen/deploy_workers.sh
+++ paper/multigpu_gen/gen/deploy_workers.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #SBATCH --job-name=wh_bnchmk
 #SBATCH --nodes=1
 #SBATCH -c 16
@@ -13,10 +12,11 @@
 #SBATCH --array=0-2
 
 echo "This is a Synthseg generation job running on node $(hostname)"
+echo "Project Name: $PROJECT_NAME"
+echo "Experiment ID: $EXPERIMENT_ID"
 echo "Error output test" >&2
 
-#export PYTHONPATH=/data/users1/mdoan4/wirehead:$PYTHONPATH
-#source /trdapps/linux-x86_64/envs/plis_conda/bin/activate /trdapps/linux-x86_64/envs/plis_conda/envs/synthseg_38
 source /data/users1/mdoan4/wirehead/paper/train/wirehead/bin/activate
 
-stdbuf -o0 python /data/users1/mdoan4/wirehead/paper/multigpu_gen/gen/worker.py
+# Pass PROJECT_NAME and EXPERIMENT_ID to the Python script
+stdbuf -o0 python /data/users1/mdoan4/wirehead/paper/multigpu_gen/gen/worker.py --experiment_id "$EXPERIMENT_ID"
