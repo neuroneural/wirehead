9fab09f36bcb45ce69660a1dd8974cf9f1f70c9b
spikedoanz <spikedoanz@gmail.com>
2024-06-25 15:44:34 +0000

    multigen 10xa40 works:

--- paper/multigpu_gen/run.sh
+++ paper/multigpu_gen/run.sh
@@ -6,6 +6,9 @@ EXPERIMENT_ID=$(date +"%Y-%m-%d_%H-%M")
 export PROJECT_NAME=$PROJECT_NAME
 export EXPERIMENT_ID=$EXPERIMENT_ID
 
+cat gen/deploy_workers.sh
+cat conf/wirehead_config.yaml
+
 # Run the training script with these hardware configs
 srun -p qTRDGPUH -A psy53c17 -v -t600 -N1-1 -c16 --gres=gpu:A100:1 --mem=200g --pty \
  train_for_distributed.sh --project_name $PROJECT_NAME --experiment_id $EXPERIMENT_ID &
