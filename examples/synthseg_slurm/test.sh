48eeabaed686c7f77e9601af677f8921bfcf2d4c
Mike Doan <spikedoanz@gmail.com>
2024-07-14 11:32:14 -0400

    auto cancel sbatch

--- examples/synthseg_slurm/test.sh
+++ examples/synthseg_slurm/test.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-# Function to terminate child processes
-terminate_child_processes() {
+# Function to terminate child processes and SLURM job
+terminate_processes() {
     # Send SIGTERM to all child processes
     pkill -SIGTERM -P $$
+
+    # Cancel the SLURM job if job_id is set
+    if [ ! -z "$job_id" ]; then
+        scancel $job_id
+        echo "Cancelled SLURM job $job_id"
+    fi
 }
+
 # Trap the SIGINT signal (Ctrl+C)
-trap terminate_child_processes SIGINT
+trap terminate_processes SIGINT
 
 python clean.py
 
-sbatch deploy_worker.sh &
+# Run sbatch and capture the job ID
+job_id=$(sbatch --parsable deploy_worker.sh)
+echo "Started SLURM job with ID: $job_id"
 
 sleep 30
-
 python loader.py
 
-kill -SIGINT $$
+# Terminate all processes (including the SLURM job)
+terminate_processes
+
+# Exit the script
+exit 0
