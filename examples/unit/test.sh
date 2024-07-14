364ddf10aa20e05363899bf66856c82697f8d43d
spikedoanz <spikedoanz@gmail.com>
2024-07-14 15:24:48 +0000

    removed logs

--- examples/unit/test.sh
+++ examples/unit/test.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# Function to terminate child processes
-terminate_child_processes() {
-    # Send SIGTERM to all child processes
-    pkill -SIGTERM -P $$
-}
-
-# Function to check if MongoDB is running
-check_mongo() {
-    if pgrep -x "mongod" > /dev/null
-    then
-        echo "MongoDB is running."
-    else
-        echo "MongoDB is not running. Please start mongod according to your system specs"
-        kill -SIGINT $$
-    fi
-}
-
-check_mongo
-
-# Trap the SIGINT signal (Ctrl+C)
 trap terminate_child_processes SIGINT
 
 python clean.py
