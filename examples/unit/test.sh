34390ea1e2e5af32d55b3455340acba9845c2395
Mike Doan <spikedoanz@gmail.com>
2024-07-15 11:42:39 -0400

    fix test script

--- examples/unit/test.sh
+++ examples/unit/test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Function to terminate child processes
+terminate_child_processes() {
+    # Send SIGTERM to all child processes
+    pkill -SIGTERM -P $$
+}
+
 trap terminate_child_processes SIGINT
 
 python clean.py
