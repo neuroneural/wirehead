69aee4d2d36bb051750caf2f03dd560dd9973572
Sergey Plis <s.m.plis@gmail.com>
2024-07-25 02:48:11 +0000

    chunking before grabbing id to reduce the gap

--- wirehead/generator.py
+++ wirehead/generator.py
@@ -9,13 +9,13 @@ import torch
 from pymongo import MongoClient, ReturnDocument
 
 
-class WireheadGenerator():
+class WireheadGenerator:
     """
     Wirehead runtime class, which wraps around the generator
     and manager runtimes.
     """
 
-    def __init__(self, generator, config_path, n_samples = 1000):
+    def __init__(self, generator, config_path, n_samples=1000):
         if config_path is None or os.path.exists(config_path) is False:
             print("No valid config specified, exiting")
             return
@@ -24,16 +24,16 @@ class WireheadGenerator():
         self.n_samples = n_samples
 
     def load_from_yaml(self, config_path):
-        """ Loads manager configs from config_path """
-        with open(config_path, 'r', encoding='utf-8') as file:
+        """Loads manager configs from config_path"""
+        with open(config_path, "r", encoding="utf-8") as file:
             config = yaml.safe_load(file)
-        dbname = config.get('DBNAME')
-        mongohost = config.get('MONGOHOST')
-        port = config.get('PORT') if config.get('PORT') is not None else 27017
+        dbname = config.get("DBNAME")
+        mongohost = config.get("MONGOHOST")
+        port = config.get("PORT") if config.get("PORT") is not None else 27017
         client = MongoClient("mongodb://" + mongohost + ":" + str(port))
 
         self.db = client[dbname]
-        self.swap_cap = config.get('SWAP_CAP')
+        self.swap_cap = config.get("SWAP_CAP")
         self.sample = tuple(config.get("SAMPLE"))
         self.chunksize = config.get("CHUNKSIZE")
         self.collectionw = config.get("WRITE_COLLECTION") + ".bin"
@@ -81,46 +81,52 @@ class WireheadGenerator():
         kinds = self.sample
         for i, kind in enumerate(kinds):
             chunks += list(
-                chunk_binobj(tensor2bin(torch.from_numpy(binobj[i])), index, kind,
-                             self.chunksize))
+                chunk_binobj(
+                    tensor2bin(torch.from_numpy(binobj[i])),
+                    index,
+                    kind,
+                    self.chunksize,
+                )
+            )
         return chunks
 
     def push_chunks(self, chunks):
-        """ Pushes chunkified tensors to mongodb, with error handling"""
+        """Pushes chunkified tensors to mongodb, with error handling"""
         collection_bin = self.db[self.collectionw]
         try:
             collection_bin.insert_many(chunks)
         except Exception as exception:
-            print(f"Generator: An error occurred: {exception}, are you swapping?")
+            print(
+                f"Generator: An error occurred: {exception}, are you swapping?"
+            )
             time.sleep(1)
 
     def get_current_idx(self):
-        """ Get current index of sample in write collection """
+        """Get current index of sample in write collection"""
         dbc = self.db[self.collectionc]
         counter_doc = dbc.find_one_and_update(
             {"_id": "uniqueFieldCounter"},
-            {"$inc": {
-                "sequence_value": 1
-            }},
+            {"$inc": {"sequence_value": 1}},
             return_document=ReturnDocument.BEFORE,
         )
         return counter_doc["sequence_value"]
 
     def generate_and_insert(self):
-        """ Fetch from generator and inserts into mongodb """
+        """Fetch from generator and inserts into mongodb"""
         # 0. Fetch data from generator
         data = next(self.generator)
-        # 1. Get the correct index for this current sample
+        # 1. Turn the data into a list of serialized chunks with fake id
+        chunks = self.chunkify(data, 0)
+        # 2. Get the correct index for this current sample
         index = self.get_current_idx()
         if index < self.swap_cap:
-            # 2. Turn the data into a list of serialized chunks
-            chunks = self.chunkify(data, index)
+            branded_chunks = [{**d, "id": index} for d in chunks]
             # 3. Push to mongodb + error handling
-            self.push_chunks(chunks)
+            self.push_chunks(branded_chunks)
 
     def run_generator(self):
-        """ Initializes and runs a SynthSeg brain generator in a loop,
-            preprocesses, then pushes to mongoDB"""
+        """Initializes and runs a SynthSeg brain generator in a loop,
+        preprocesses, then pushes to mongoDB"""
         print("Generator: Initialized")
         n_samples = self.n_samples
         for _ in range(n_samples):
