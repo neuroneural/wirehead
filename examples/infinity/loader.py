ea7e74db700f05f1fd5ce8697764ef063fcb9d61
Mike Doan <spikedoanz@gmail.com>
2024-08-19 12:12:43 -0400

    gca

--- examples/infinity/loader.py
+++ examples/infinity/loader.py
@@ -7,10 +7,10 @@ from wirehead import MongoTupleheadDataset
 dataset = MongoTupleheadDataset(config_path = "config.yaml")
 
 for i in range(1000000):
-    idx = [i%100]
-    data = dataset[idx]
-    sample, label = data[0][0], data[0][1]
-    print("Loader, index: ", i)
-    time.sleep(0.1)
+    for j in range(10):
+        idx = [i%20]
+        data = dataset[idx]
+        sample, label = data[0][0], data[0][1]
+        print("Loader, index: ", idx)
 
 print("Unit example passed successfully")
