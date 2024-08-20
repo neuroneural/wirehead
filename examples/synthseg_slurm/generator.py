474b64feb1857e06b42a52a8afdc2fae6c28c39e
Mike Doan <spikedoanz@gmail.com>
2024-08-20 14:41:25 -0400

    less violent generator scripts

--- examples/synthseg_slurm/generator.py
+++ examples/synthseg_slurm/generator.py
@@ -1,4 +1,4 @@
-from time import time
+from glob import glob
 import gc
 import os
 from nobrainer.processing.brain_generator import BrainGenerator
@@ -13,13 +13,19 @@ os.environ["TF_AUTOGRAPH_VERBOSITY"] = "0"
 import tensorflow as tf
 
 WIREHEAD_CONFIG = "config.yaml"
-DATA_FILES = ["example.nii.gz"]
-NUM_GENERATORS = 1
+DATA_FILES = glob(
+    "/home/users/splis/craft/meshnet/enmesh2/synthseg_labels/*nii.gz"
+)
+DATA_FILES += glob("/data/users2/tle89/here/1m/*nii.gz")
+DATA_FILES += glob("/data/users2/tle89/here/3m/*nii.gz")
+DATA_FILES += glob("/data/users2/tle89/here/6m/*nii.gz")
+DATA_FILES += glob("/data/users2/tle89/clinicalHead_result3/*nii.gz")
+
 
 physical_devices = tf.config.list_physical_devices("GPU")
 try:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
-except:
+except Exception:
     pass
 
 
@@ -32,7 +38,7 @@ def create_generator(file_id=0):
     training_seg = DATA_FILES[file_id]
     brain_generator = BrainGenerator(
         training_seg,
-        randomise_res=False,
+        randomise_res=True,
     )
     print(f"Generator {file_id}: SynthSeg is using {training_seg}", flush=True)
     while True:
@@ -41,16 +47,24 @@ def create_generator(file_id=0):
         gc.collect()
 
 
-def run_wirehead_generator(file_id):
-    brain_generator = create_generator(file_id)
-    wirehead_generator = WireheadGenerator(
-        generator=brain_generator, config_path=WIREHEAD_CONFIG
-    )
-    wirehead_generator.run()
+def run_wirehead_generator(get_file_id, n_samples=10000):
+    # or whatever 15 minutes equals
+    idx = 0
+    while True:
+        brain_generator = create_generator(get_file_id(idx))
+        wirehead_generator = WireheadGenerator(
+            generator=brain_generator, config_path=WIREHEAD_CONFIG
+        )
+        for i in range(n_samples):
+            wirehead_generator.cycle()
+        del brain_generator
+        gc.collect()  # i am paranoid
+        idx += 1
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run wirehead generators")
+    parser.add_argument("num_tasks", type=int, help="Number of running tasks")
     parser.add_argument(
         "num_generators", type=int, help="Number of generators to run"
     )
@@ -59,11 +73,21 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    slurm_job_id = int(os.environ.get("SLURM_JOB_ID"))
+    if slurm_job_id := os.environ.get("SLURM_JOB_ID"):
+        slurm_job_id = int(slurm_job_id)
+    else:
+        print("Not in a slurm job, exiting")
+        exit()
+
+    def get_file_id(idx):
+        return (
+            (slurm_job_id + idx * args.num_tasks) * args.num_generators
+            + args.generator_id
+        ) % len(DATA_FILES)
 
     # Calculate file index
     file_idx = (slurm_job_id * args.num_generators + args.generator_id) % len(
         DATA_FILES
     )
     # Run the generator
-    run_wirehead_generator(file_idx)
+    run_wirehead_generator(get_file_id)
